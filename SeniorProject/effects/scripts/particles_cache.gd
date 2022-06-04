58166ecf62a52c867bf1f9ea836368a4d9aae5fb
Evan <eyoussef128@gmail.com>
2022-06-03 22:33:15 -0400

    Update particles_cache.gd
    Adjusted particle cache position

--- SeniorProject/effects/scripts/particles_cache.gd
+++ SeniorProject/effects/scripts/particles_cache.gd
@@ -18,7 +18,7 @@ var particles = [
 func ready():
 	for i in particles:
 		var particles_instance = Particles2D.new()
-		particles_instance.position = Vector2(-64, 64)
+		particles_instance.position = Vector2(-64, -64)
 		particles_instance.set_process_material(particles)
 		particles_instance.set_one_shot(true)
 		particles_instance.set_emitting(true)
